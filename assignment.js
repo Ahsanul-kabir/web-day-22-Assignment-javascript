c143d35866b6abd6a8972d35728e08f960ac396d
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-22 12:34:13 +0600

    update with bonus part  till hotelCost Part

--- assignment.js
+++ assignment.js
@@ -1,49 +1,90 @@
+// Source Code: https://github.com/Ahsanul-kabir/web-day-22-Assignment-javascript
+
+
+
+
 // kilometerToMeter
 function kilometerToMeter(km) {
-    var meter = km * 1000;
-    return meter;
+
+    if (km < 0) {
+        return "You Can't give negative value";
+    } else if ((km >= 'a' || km <= 'z') || (km >= 'A' || km <= 'Z')) {
+        return "You Can't give String value";
+    } else {
+        // main logic phase
+        var meter = km * 1000;
+        return meter;
+    }
 }
-var resultKM = kilometerToMeter(7);
+var resultKM = kilometerToMeter("aa"); // take return
 console.log(resultKM);
+
+
+
+
 
 
 // budgetCalculator
 function budgetCalculator(watchH, phoneH, laptopH) {
-    var watchPrice = 50;
-    var phonePrice = 100;
-    var laptopPrice = 500;
-    var resultWatch = watchPrice * watchH;
-    var resultPhone = phonePrice * phoneH;
-    var resultLaptop = laptopPrice * laptopH;
 
-    total = resultWatch + resultPhone + resultLaptop;
-    return total;
+    if (watchH < 0 || phoneH < 0 || laptopH < 0) {
+        return "You Can't give negative value";
+    } else if ((watchH >= 'a' || watchH <= 'z') || (watchH >= 'A' || watchH <= 'Z')) {
+        return "You Can't give String value";
+    } else if ((phoneH >= 'a' || phoneH <= 'z') || (phoneH >= 'A' || phoneH <= 'Z')) {
+        return "You Can't give String value";
+    } else if ((laptopH >= 'a' || laptopH <= 'z') || (laptopH >= 'A' || laptopH <= 'Z')) {
+        return "You Can't give String value";
+    } else {
+        // main logic phase
+        var watchPrice = 50;
+        var phonePrice = 100;
+        var laptopPrice = 500;
+        var resultWatch = watchPrice * watchH;
+        var resultPhone = phonePrice * phoneH;
+        var resultLaptop = laptopPrice * laptopH;
+
+        total = resultWatch + resultPhone + resultLaptop;
+        return total;
+    }
+
 }
-var resultBudget = budgetCalculator(1, 2, 5);
+var resultBudget = budgetCalculator("aa", "aa", "aa"); // take return
 console.log(resultBudget);
+
+
+
 
 
 // hotelCost
 function hotelCost(days) {
-    var noDiscountPhese = 100;
-    var discountPhese1 = 80;
-    var discountPhese2 = 50;
-    var totalRentCost = 0;
 
-    if (days >= 1 && days <= 10) {
-        var totalRentCost = days * noDiscountPhese;
-    } else if (days >= 11 && days <= 20) {
-        var rent10_days = noDiscountPhese * 10;
-        var bonusDay = days - 10;
-        var rent10_up_days = bonusDay * discountPhese1;
-        var totalRentCost = rent10_days + rent10_up_days;
+    if (days < 0) {
+        return "You Can't give negative value";
+    } else if ((days >= 'a' || days <= 'z') || (days >= 'A' || days <= 'Z')) {
+        return "You Can't give String value";
     } else {
-        var rent20_days = (noDiscountPhese * 10) + (discountPhese1 * 10);
-        var bonusDay = days - 20;
-        var rent20_up_days = bonusDay * discountPhese2;
-        var totalRentCost = rent20_days + rent20_up_days;
+        // main logic phase
+        var noDiscountPhese = 100;
+        var discountPhese1 = 80;
+        var discountPhese2 = 50;
+        var totalRentCost = 0;
+
+        if (days >= 1 && days <= 10) {
+            var totalRentCost = days * noDiscountPhese;
+        } else if (days >= 11 && days <= 20) {
+            var rent10_days = noDiscountPhese * 10;
+            var bonusDay = days - 10;
+            var rent10_up_days = bonusDay * discountPhese1;
+            var totalRentCost = rent10_days + rent10_up_days;
+        } else {
+            var rent20_days = (noDiscountPhese * 10) + (discountPhese1 * 10);
+            var bonusDay = days - 20;
+            var rent20_up_days = bonusDay * discountPhese2;
+            var totalRentCost = rent20_days + rent20_up_days;
+        }
+        return totalRentCost;
     }
-    return totalRentCost;
 }
-var resultHotelCost = hotelCost(22);
+var resultHotelCost = hotelCost("aa"); // take return
 console.log(resultHotelCost);
