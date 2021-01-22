a49fdf365335196d965c5a5e29b42b74a3430b50
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-22 10:17:33 +0600

    hotelCost Part added

--- assignment.js
+++ assignment.js
@@ -23,4 +23,26 @@ var resultBudget = budgetCalculator(1, 2, 5);
 console.log(resultBudget);
 
 
-//hotelCost()
+// hotelCost
+function hotelCost(days) {
+    noDiscountPhese = 100;
+    discountPhese1 = 80;
+    discountPhese2 = 50;
+
+    if (days >= 1 && days <= 10) {
+        var rent = days * noDiscountPhese;
+    } else if (days >= 11 && days <= 20) {
+        rent10_days = noDiscountPhese * 10;
+        bonusDay = days - 10;
+        var rent10_up_days = bonusDay * discountPhese1;
+        var rent = rent10_days + rent10_up_days;
+    } else {
+        rent20_days = (noDiscountPhese * 10) + (discountPhese1 * 10);
+        bonusDay = days - 20;
+        var rent20_up_days = bonusDay * discountPhese2;
+        var rent = rent20_days + rent20_up_days;
+    }
+    return rent;
+}
+var resultHotelCost = hotelCost(22);
+console.log(resultHotelCost);
