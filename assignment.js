2993d8f30b85a6f793f32506837e680be267abda
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-22 23:35:17 +0600

    assignment complite

--- assignment.js
+++ assignment.js
@@ -1,9 +1,10 @@
-// Source Code: https://github.com/Ahsanul-kabir/web-day-22-Assignment-javascript
+// https://github.com/Ahsanul-kabir/web-day-22-Assignment-javascript
 
 
-// kilometerToMeter
+// 1------ kilometerToMeter
 function kilometerToMeter(km) {
 
+    // check valid or not
     if (km < 0) {
         return "WARNING: You Can't give negative value!";
     } else if ((km >= 'a' || km <= 'z') || (km >= 'A' || km <= 'Z')) {
@@ -15,56 +16,62 @@ function kilometerToMeter(km) {
         return meter;
     }
 }
-var resultKM = kilometerToMeter(1.2); // take return
+var resultKM = kilometerToMeter(7);
 console.log(resultKM);
 
 
 
-// budgetCalculator
-function budgetCalculator(watchH, phoneH, laptopH) {
+// 2------- budgetCalculator
+function budgetCalculator(watchWant, phoneWant, laptopWant) {
 
-    if (watchH < 0 || phoneH < 0 || laptopH < 0) {
+    // check valid or not
+    if (watchWant < 0 || phoneWant < 0 || laptopWant < 0) {
         return "WARNING: You Can't give negative value and String!";
-    } else if ((watchH >= 'a' || watchH <= 'z') || (watchH >= 'A' || watchH <= 'Z')) {
+    } else if ((watchWant >= 'a' || watchWant <= 'z') || (watchWant >= 'A' || watchWant <= 'Z')) {
         return "WARNING: You Can't give String value!";
-    } else if ((phoneH >= 'a' || phoneH <= 'z') || (phoneH >= 'A' || phoneH <= 'Z')) {
+    } else if ((phoneWant >= 'a' || phoneWant <= 'z') || (phoneWant >= 'A' || phoneWant <= 'Z')) {
         return "WARNING: You Can't give String value!";
-    } else if ((laptopH >= 'a' || laptopH <= 'z') || (laptopH >= 'A' || laptopH <= 'Z')) {
+    } else if ((laptopWant >= 'a' || laptopWant <= 'z') || (laptopWant >= 'A' || laptopWant <= 'Z')) {
         return "WARNING: You Can't give String value!";
     } else {
-        // main logic phase
+        // given price
         var watchPrice = 50;
         var phonePrice = 100;
         var laptopPrice = 500;
-        var resultWatch = watchPrice * watchH;
-        var resultPhone = phonePrice * phoneH;
-        var resultLaptop = laptopPrice * laptopH;
 
-        total = resultWatch + resultPhone + resultLaptop;
-        return total;
+        // calculate indidual price
+        var resultWatch = watchPrice * watchWant;
+        var resultPhone = phonePrice * phoneWant;
+        var resultLaptop = laptopPrice * laptopWant;
+
+        var totalCost = resultWatch + resultPhone + resultLaptop; // total cost
+        return totalCost;
     }
 
 }
-var resultBudget = budgetCalculator(10, 20, 0); // take return
+var resultBudget = budgetCalculator(2, 5, 3);
 console.log(resultBudget);
 
 
 
 
-// hotelCost
+// 3---------hotelCost
 function hotelCost(days) {
 
+    // check valid or not
     if (days < 0) {
         return "WARNING: You Can't give negative value!";
     } else if ((days >= 'a' || days <= 'z') || (days >= 'A' || days <= 'Z')) {
         return "WARNING: You Can't give String value!";
     } else {
-        // main logic phase
+
+        // given distance
         var noDiscountPhese = 100;
         var discountPhese1 = 80;
         var discountPhese2 = 50;
         var totalRentCost = 0;
 
+        // calculate cost
         if (days >= 1 && days <= 10) {
             var totalRentCost = days * noDiscountPhese;
         } else if (days >= 11 && days <= 20) {
@@ -81,25 +88,30 @@ function hotelCost(days) {
         return totalRentCost;
     }
 }
-var resultHotelCost = hotelCost(12); // take return
+var resultHotelCost = hotelCost(15);
 console.log(resultHotelCost);
 
 
-// megaFriend
+
+
+// 4----------megaFriend
 function megaFriend(names) {
 
-    // check given friend list is empty or not.
+    // check given friend list is empty or not
     if (names.length === 0) {
         return "WARNING: No friends listed!";
     } else {
-        // initialize the first name as megaFriendName.
+        // initialize the first name as megaFriendName
         var megaFriendName = names[0];
 
         for (var i = 1; i < names.length; i++) {
+            // check valid or not
             if (typeof names[i] != 'string') {
-                return "WARNING: You have to give String value!";
+                return "WARNING: You have to give String value all the parameter!";
             }
-            // change megaFriendName if find a larger one
+
+
+            // find big length of name
             if (megaFriendName.length < names[i].length) {
                 megaFriendName = names[i];
             }
