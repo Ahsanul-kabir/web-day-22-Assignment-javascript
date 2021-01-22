0280d27eb4e1ceb09bc038433e8c10f65a76232c
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-22 09:28:36 +0600

    budget Part added

--- assignment.js
+++ assignment.js
@@ -5,3 +5,19 @@ function kilometerToMeter(km) {
 }
 var resultKM = kilometerToMeter(7);
 console.log(resultKM);
+
+
+// budgetCalculator
+function budgetCalculator(watchH, phoneH, laptopH) {
+    var watchPrice = 50;
+    var phonePrice = 100;
+    var laptopPrice = 500;
+    var resultWatch = watchPrice * watchH;
+    var resultPhone = phonePrice * phoneH;
+    var resultLaptop = laptopPrice * laptopH;
+
+    total = resultWatch + resultPhone + resultLaptop;
+    return total;
+}
+var resultBudget = budgetCalculator(1, 2, 5);
+console.log(resultBudget);
